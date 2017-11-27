c2d89cb6666e4c15696e3ffc2c44fcc606d0ccb3
Unknown <mymintzata@hotmail.co.th>
2017-11-27 18:09:05 +0700

    ทำข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss.Worarat Panbrmrungsuk" << endl;
+  cout << "Nickname : Mint" << endl;
+  cout << "sex : female" << endl;
+  cout << "Birthday : 10 may 1999" << endl;
+  cout << "Hobbies : Listen to music" << endl;
+  cout << "Status : Single" << endl;
+
   return 0;
 }
