5a6794959ae4b9597499f944619be0beccc641d1
Unknown <mymintzata@hotmail.co.th>
2017-11-28 11:49:12 +0700

    ทำข้อที่ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main() {
-  
+
+    const float pi = 3.14315926536;
+    float r;
+    float area;
+    int h=15;
+
+//วงกลม
+    cout << "Please enter radius : ";
+    cin >> r;
+    area = pi*r*r;
+    cout << "Area of Circle :" << area << endl;
+//เส้นรอย
+    cout << "Formula for circular circumference"<< endl;
+    cout << "Area of Circle : " << 2*pi*r<< endl;
+//ทรงกระบอก
+    cout << "15 high cylinder formula"<< endl;
+    cout << "Area of Circle :" << pi*(r*r)*h<< endl;
+
+
+
+
+
+
+
+
   return 0;
 }
